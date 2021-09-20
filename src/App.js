59a2357b9d9e8f96d4ef06d2e3632396776e91d5
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-09-20 19:20:53 +0300

    Add tasks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import Header from './components/Header';
 import './App.css';
 import Main from './components/Main';
 
+const user = {
+  firstName: 'Damian',
+  lastName: 'Wayne',
+  age: 22,
+  email: 'damianwayne22@gmail.com',
+  description: 'test information' 
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main user={user}/>
     </div>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,12 @@
-const test = <>Hi</>
-
-function Main () {
-    return (test);
+function Main(props) {
+    const {firstName, lastName, email} = props.user;
+  return (
+    <div>
+      <div>{firstName}</div>
+      <div>{lastName}</div>
+      <div>{email}</div>
+    </div>
+  );
 }
 
 export default Main;
